673a754e93775ff6d05b269422a96e1b6ef9ab91
Jay-Jay-Cho <whalswo7777@gmail.com>
2020-08-14 17:30:47 +0900

    blah blah

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const axios = require("axios");
 const params = { 'a': 1, 'b': 2 };
 
-axios.post("https://relay14-server.herokuapp.com/",params)
-.then((response) => {
+// axios.post("https://relay14-server.herokuapp.com/", params)
+//     .then((response) => {
+//         console.log(response);
+//     }, (error) => {
+//         console.log(error);
+//     });
+
+axios({
+    method: 'post',
+    url: 'https://relay14-server.herokuapp.com/',
+    data: params
+}).then((response) => {
     console.log(response);
 }, (error) => {
     console.log(error);
-});
+});;
