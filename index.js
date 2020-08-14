459dbd7f6e0b879baafd9541227269936bd12d95
Jay-Jay-Cho <whalswo7777@gmail.com>
2020-08-14 17:16:13 +0900

    add calculation

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 let express = require("express");
+const axios = require("axios");
 let app = express();
 
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT, function () {
     console.log("App is running on port");
 });
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.sendfile("index.html");
 });
+
+
+
+const params = { 'a': 1, 'b': 2 };
+
+axios.post("https://relay14-server.herokuapp.com/",params)
+.then((response) => {
+    console.log(response);
+}, (error) => {
+    console.log(error);
+});
+
